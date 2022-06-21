c7e5b6e4785130225fa52dd700649236151ce3a0
parksm2 <parksm2@brandi.co.kr>
2022-06-21 12:42:19 +0900

    init

---
+++ WTFSwiftUI/Pods/Target Support Files/Pods-WTFSwiftUI/Pods-WTFSwiftUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_WTFSwiftUI : NSObject
+@end
+@implementation PodsDummy_Pods_WTFSwiftUI
+@end
---
+++ WTFSwiftUI/WTFSwiftUI/WTFSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTFSwiftUIApp.swift
+//  WTFSwiftUI
+//
+//  Created by 박성민 on 2022/06/21.
+//
+
+import SwiftUI
+
+@main
+struct WTFSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
